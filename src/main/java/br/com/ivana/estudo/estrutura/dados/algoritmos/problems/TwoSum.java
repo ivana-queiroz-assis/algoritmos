363419120c7492c6ba02f13c28ef4e23873e8044
Ivana Queiroz de Assis <ivanaqueirozassis@hotmail.com>
2019-07-27 10:18:02 -0300

    More problems 2.

--- src/main/java/br/com/ivana/estudo/estrutura/dados/algoritmos/problems/TwoSum.java
+++ src/main/java/br/com/ivana/estudo/estrutura/dados/algoritmos/problems/TwoSum.java
@@ -30,31 +30,31 @@ public class TwoSum {
 	//Time complexity : O(n)O(n). We traverse the list containing nn elements exactly twice. Since the hash table reduces the look up time to O(1)O(1), the time complexity is O(n)O(n).
 	//
 	//Space complexity : O(n)O(n). The extra space required depends on the number of items stored in the hash table, which stores exactly nn elements.
-	public int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < nums.length; i++) {
-			map.put(nums[i], i);
-		}
-		for (int i = 0; i < nums.length; i++) {
-			int complement = target - nums[i];
-			if (map.containsKey(complement) && map.get(complement) != i) {
-				return new int[] { i, map.get(complement) };
-			}
-		}
-		throw new IllegalArgumentException("No two sum solution");
-	}
+//	public int[] twoSum(int[] nums, int target) {
+//		Map<Integer, Integer> map = new HashMap<>();
+//		for (int i = 0; i < nums.length; i++) {
+//			map.put(nums[i], i);
+//		}
+//		for (int i = 0; i < nums.length; i++) {
+//			int complement = target - nums[i];
+//			if (map.containsKey(complement) && map.get(complement) != i) {
+//				return new int[] { i, map.get(complement) };
+//			}
+//		}
+//		throw new IllegalArgumentException("No two sum solution");
+//	}
 //Time complexity : O(n)O(n). We traverse the list containing nn elements only once. Each look up in the table costs only O(1)O(1) time.
 //
 //Space complexity : O(n)O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements
-	public int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < nums.length; i++) {
-			int complement = target - nums[i];
-			if (map.containsKey(complement)) {
-				return new int[] { map.get(complement), i };
-			}
-			map.put(nums[i], i);
-		}
-		throw new IllegalArgumentException("No two sum solution");
-	}
+//	public int[] twoSum(int[] nums, int target) {
+//		Map<Integer, Integer> map = new HashMap<>();
+//		for (int i = 0; i < nums.length; i++) {
+//			int complement = target - nums[i];
+//			if (map.containsKey(complement)) {
+//				return new int[] { map.get(complement), i };
+//			}
+//			map.put(nums[i], i);
+//		}
+//		throw new IllegalArgumentException("No two sum solution");
+//	}
 }
