29bfafa04754d0ed44317bc72a21f0ae8f69ffc7
ivana.assis <ivana.assis@unisys.com>
2019-07-29 13:47:26 -0300

    Problema do array Uber Produto.

---
+++ src/main/java/br/com/ivana/estudo/estrutura/dados/algoritmos/problems/UberArray.java
@@ -0,0 +1,32 @@
+package br.com.ivana.estudo.estrutura.dados.algoritmos.problems;
+
+import java.util.Arrays;
+
+public class UberArray {
+
+    public static void main(String[] args) {
+        int array[] = new int[]{1,2,3,4,5};
+
+        System.out.println(Arrays.toString(productAllOtherNumbers(array)));
+    }
+
+    /*Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.*/
+    public static int[] productAllOtherNumbers(int[] array){
+        int[] productAllOtherNumbers = new int[array.length];
+        int mult = 1;
+        for (int i=0;i<array.length;i++){
+            for (int j=0;j<array.length;j++){
+                if(i!= j){
+                   mult*=array[j];
+                }
+            }
+            productAllOtherNumbers[i] = mult;
+            mult=1;
+        }
+        return productAllOtherNumbers;
+    }
+
+    public static int[] productAllOtherNumbersRecursive(int[] array){
+
+    }
+}
